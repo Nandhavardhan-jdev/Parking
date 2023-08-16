aa7c301f6ce6b9b22f036b436d6a7035e6a40ab2
Nandhavardhan-jdev <141902336+Nandhavardhan-jdev@users.noreply.github.com>
2023-08-16 11:19:26 +0530

    Update TitanAruParkingApplication.java

--- titan_aru_parking/src/main/java/com/aruparking/TitanAruParkingApplication.java
+++ titan_aru_parking/src/main/java/com/aruparking/TitanAruParkingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TitanAruParkingApplication {
 
-	//checking 2nd
 	public static void main(String[] args) {
 		SpringApplication.run(TitanAruParkingApplication.class, args);
 	}
