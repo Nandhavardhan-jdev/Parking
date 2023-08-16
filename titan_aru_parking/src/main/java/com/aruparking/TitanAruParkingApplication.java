e87356bfc0957a307a53cb8b616f819e05423e30
Nandha <nandhavardhan.g@dikshatech.com>
2023-08-16 10:46:27 +0530

    checking

--- titan_aru_parking/src/main/java/com/aruparking/TitanAruParkingApplication.java
+++ titan_aru_parking/src/main/java/com/aruparking/TitanAruParkingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TitanAruParkingApplication {
 
+	//checking 1st
 	public static void main(String[] args) {
 		SpringApplication.run(TitanAruParkingApplication.class, args);
 	}
